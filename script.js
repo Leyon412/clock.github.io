9617649cd25876d4b8ab5c8ab2011782e444c0f8
Leyon412 <98469977+Leyon412@users.noreply.github.com>
2025-06-11 13:26:42 +0530

    uploaded

---
+++ script.js
@@ -0,0 +1,24 @@
+const formatter = new Intl.DateTimeFormat("en-IN", {
+  timeZone: "Asia/Kolkata",
+  hour: "2-digit",
+  minute: "2-digit",
+  hour12: true
+});
+
+const parts = formatter.formatToParts(new Date());
+
+let hour, minute, dayPeriod;
+
+for (const part of parts) {
+  if (part.type === "hour") hour = part.value;
+  if (part.type === "minute") minute = part.value;
+  if (part.type === "dayPeriod") dayPeriod = part.value;
+}
+
+console.log("Hour:", hour);
+console.log("Minute:", minute);
+console.log("AM/PM:", dayPeriod);
+
+document.getElementById('inner1').textContent=hour;
+document.getElementById('inner2').textContent=minute;
+document.getElementById('ti').textContent=dayPeriod;
